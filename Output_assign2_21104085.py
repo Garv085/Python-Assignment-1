f3da99e5212eeea89fe12ef0d0f4911fea4e66c3
Garv085 <97451288+Garv085@users.noreply.github.com>
2022-01-26 17:52:09 +0530

    Add files via upload
    python assignment 2 
    garv bansal
    21104085

---
+++ Output_assign2_21104085.py
@@ -0,0 +1,39 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: /Users/garvbansal/Desktop/Python/assignment2_21104085.py ======
+#Ques_1
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+#Ques_2
+Hey, Garv Bansal Here!
+My SID is 21104085
+I am from EE department and my CGPA is 9.9
+#Ques_3
+8
+58
+50
+224
+40
+14
+0
+#Ques_4
+Enter first number: 4
+Enter second number: 45
+Enter third number: 1001
+The largest number is 1001.0
+#Ques_5
+enter anything u want: my name garv bansal
+No
+Yes
+No
+No
+#Ques_6
+Enter the 1st side:  3.1
+Enter the 2nd side:  2.5
+Enter the 3rd side:  7.1
+No
